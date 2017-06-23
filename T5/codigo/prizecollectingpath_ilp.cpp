33411130d2ae08d84c8cce694dd98468744f4e4b
Fabio Takahashi Tanniguchi <ra145980@drummond.lab.ic.unicamp.br>
2017-06-23 17:05:27 -0300

    Setando parametro de cutoff

--- T5/codigo/prizecollectingpath_ilp.cpp
+++ T5/codigo/prizecollectingpath_ilp.cpp
@@ -84,11 +84,15 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
 		GRBModel model = GRBModel(env);
 		GRBLinExpr expr;
 		model.set(GRB_IntAttr_ModelSense, GRB_MAXIMIZE);
-		env.set(GRB_DoubleParam_TimeLimit, tMax);
+		cout << "Premio total: " << premioTotal << "\n";
+		
+		// o modelo ja comeca com o valor do premio da solucao gulosa como parametro de cutoff
+		model.getEnv().set(GRB_DoubleParam_Cutoff, premioTotal - 1.0);
 		
 		// criando variaveis
 		vector<GRBVar> x(countArcs(g)); // variavel que indica se a aresta eh usada
 		for(ListDigraph::ArcIt arc(g); arc != INVALID; ++arc){
+			// o x[arco] ja comeca com o valor da solucao gulosa
 			x[g.id(arc)] = model.addVar(0, 1, solInicial[arc], GRB_BINARY);
 		}
 		
